3c2d6e864f826db6647468f0a29982baad2d403e
Alexrus-cyber <85308640+Alexrus-cyber@users.noreply.github.com>
2022-02-14 14:42:26 +0300

    Теперь при внутренней тасовке выполняется всего 30 запросов. Переключите программу на внешнюю тасовку, и вы заметите аналогичное улучшение: теперь выполняется 162 запроса.

--- Cards/Program.cs
+++ Cards/Program.cs
@@ -12,8 +12,10 @@ namespace Cards
         static void Main(string[] args)
         {
             var startingDeck = (from s in Suits().LogQuery("Suit Generation")
-                                from r in Ranks().LogQuery("Rank Generation")
-                                select new { Suit = s, Rank = r }).LogQuery("Starting Deck");
+                                from r in Ranks().LogQuery("Value Generation")
+                                select new { Suit = s, Rank = r })
+                        .LogQuery("Starting Deck")
+                        .ToArray();
 
             foreach (var c in startingDeck)
             {
@@ -21,24 +23,25 @@ namespace Cards
             }
 
             Console.WriteLine();
+
             var times = 0;
             var shuffle = startingDeck;
 
             do
             {
-                // Out shuffle
                 /*
                 shuffle = shuffle.Take(26)
                     .LogQuery("Top Half")
-                    .InterleaveSequenceWith(shuffle.Skip(26)
-                    .LogQuery("Bottom Half"))
-                    .LogQuery("Shuffle");
+                    .InterleaveSequenceWith(shuffle.Skip(26).LogQuery("Bottom Half"))
+                    .LogQuery("Shuffle")
+                    .ToArray();
                 */
 
-                // In shuffle
-                shuffle = shuffle.Skip(26).LogQuery("Bottom Half")
-                        .InterleaveSequenceWith(shuffle.Take(26).LogQuery("Top Half"))
-                        .LogQuery("Shuffle");
+                shuffle = shuffle.Skip(26)
+                    .LogQuery("Bottom Half")
+                    .InterleaveSequenceWith(shuffle.Take(26).LogQuery("Top Half"))
+                    .LogQuery("Shuffle")
+                    .ToArray();
 
                 foreach (var c in shuffle)
                 {
