435e7a971d8e164a394b1689d77b56812b3199dd
Alexrus-cyber <85308640+Alexrus-cyber@users.noreply.github.com>
2022-02-14 14:31:37 +0300

    Но теперь вместо команды yield return, которая возвращает элементы, вы будете сравнивать элементы каждой последовательности.

--- Cards/Extensions.cs
+++ Cards/Extensions.cs
@@ -7,17 +7,21 @@ namespace Cards
 {
     public static class Extensions
     {
-        public static IEnumerable<T> InterleaveSequenceWith<T>
-   (this IEnumerable<T> first, IEnumerable<T> second)
+        public static bool SequenceEquals<T>
+    (this IEnumerable<T> first, IEnumerable<T> second)
         {
             var firstIter = first.GetEnumerator();
             var secondIter = second.GetEnumerator();
 
             while (firstIter.MoveNext() && secondIter.MoveNext())
             {
-                yield return firstIter.Current;
-                yield return secondIter.Current;
+                if (!firstIter.Current.Equals(secondIter.Current))
+                {
+                    return false;
+                }
             }
+
+            return true;
         }
     }
 }
